762f1704a1db84ce01ec813539305b6b489eb93a
hayala87 <heri8706@gmail.com>
2025-07-08 11:11:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,7 @@ def webhook():
     return jsonify({"reply": bot_reply})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
